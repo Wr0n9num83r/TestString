47e2a79797811df1b71eacbb5795f2b0b6a85a80
Piotr <piotrbernys11@gmail.com>
2023-11-15 09:36:51 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj ciąg znaków: ");
+        String znaki = scanner.nextLine();
 
-
-
+        String koniec = "";
+        if(znaki.length() >= 3){
+            koniec = znaki.substring(znaki.length()-2);
+            znaki = znaki.substring(0,znaki.length()-2);
+            System.out.println(koniec + znaki);
+        }
+        else{
+            System.out.println("Ciąg znaków musi składać się z minimum 3 znaków.");
+        }
     }
 }
